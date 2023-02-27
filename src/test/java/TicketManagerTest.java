ea80d7fd58ed6d51860b87f017afbca87cccc770
Evgeniy Popov <evgenij.selin.94@bk.ru>
2023-02-27 18:18:53 +0300

    Change test commit

--- src/test/java/TicketManagerTest.java
+++ src/test/java/TicketManagerTest.java
@@ -23,6 +23,7 @@ public class TicketManagerTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     public void searh2() {
         TicketRepository repository = new TicketRepository();
@@ -38,6 +39,25 @@ public class TicketManagerTest {
         manager.add(ticket4);
         Ticket[] actual = manager.findAll("KZN", "SVO");
         Ticket[] expected = {ticket3};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void searh3() {
+        TicketRepository repository = new TicketRepository();
+        TicketManager manager = new TicketManager(repository);
+        Ticket ticket1 = new Ticket(1, 5000, "SVO", "KZN", "10000");
+        Ticket ticket2 = new Ticket(2, 6000, "SVO", "KZN", "11000");
+        Ticket ticket3 = new Ticket(3, 7500, "KZN", "SVO", "20000");
+        Ticket ticket4 = new Ticket(4, 4000, "SVO", "KZN", "15000");
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        Ticket[] actual = manager.findAll("SPB", "MSK");
+        Ticket[] expected = {};
 
         Assertions.assertArrayEquals(expected, actual);
     }
